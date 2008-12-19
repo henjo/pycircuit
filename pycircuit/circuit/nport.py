f6b423a36bbf66401854c2723b4e4a7e448329c3
Henrik Johansson <henrik@rigel.johome.net>
2008-12-19 16:17:00 +0100

    Moved n-port analyses to a separate file, wrote testcases for nport.py

--- pycircuit/circuit/nport.py
+++ pycircuit/circuit/nport.py
@@ -2,9 +2,6 @@
 # See LICENSE for details.
 
 import numpy as npy
-from circuit import SubCircuit, gnd, R, VS, IS
-from analysis import Analysis, AC, Noise
-from pycircuit.post.internalresult import InternalResultDict
 from copy import copy
 import constants
 
@@ -62,7 +59,7 @@ class NPort(object):
         
         return self.__class__(znport)
 
-    def noisy_passive_nport(self, T=290):
+    def noisy_nport(self, T=290):
         """Returns an n-port with noise parameters set if passive"""
 
         if not self.passive:
@@ -80,13 +77,14 @@ class NPortY(NPort):
     def __init__(self, Y, CY = None):
         if isinstance(Y, NPort):
             self.Y = Y.Y
+            self.CY = Y.CY
         else:
             self.Y = Y
         
-        if CY == None:
-            self.CY = npy.zeros(npy.shape(self.Y))
-        else:
-            self.CY = CY
+            if CY == None:
+                self.CY = npy.zeros(npy.shape(self.Y))
+            else:
+                self.CY = CY
 
         self.n = npy.size(self.Y,0)
         
@@ -104,12 +102,12 @@ class NPortY(NPort):
     @property
     def Z(self):
         """Return Z-parameter matrix"""
-        return npy.invert(self.Y)
+        return npy.linalg.inv(self.Y)
 
     @property
     def S(self, z0 = 50.0):
         """Return scattering parameters"""
-        
+        Y = self.Y
         E = npy.mat(npy.eye(self.n, self.n))
         Zref = z0 * E
         Gref = 1 / npy.sqrt(npy.real(z0)) * E
@@ -117,13 +115,14 @@ class NPortY(NPort):
 
     @property
     def CZ(self):
-        Z = mat(self.Z)
+        Z = npy.mat(self.Z)
         return npy.array(Z * self.CY * Z.H)
 
     @property
-    def CS(self):
+    def CS(self, z0 = 50.):
+        S = npy.mat(self.S)
         E = npy.mat(npy.eye(self.n, self.n))
-        return npy.array((E + S) * sympy.mat(self.CY) * (E + S).H / 4)
+        return npy.array((E + S) * (npy.mat(self.CY)*z0) * (E + S).H / 4)
 
     @property
     def CA(self):
@@ -139,13 +138,14 @@ class NPortZ(NPort):
     def __init__(self, Z, CZ = None):
         if isinstance(Z, NPort):
             self.Z = Z.Z
+            self.CZ = Z.CZ
         else:
             self.Z = Z
         
-        if CZ == None:
-            self.CZ = npy.zeros(npy.shape(self.Y))
-        else:
-            self.CZ = CZ
+            if CZ == None:
+                self.CZ = npy.zeros(npy.shape(self.Y))
+            else:
+                self.CZ = CZ
 
         self.n = npy.size(self.Z,0)
 
@@ -157,13 +157,13 @@ class NPortZ(NPort):
 
         Z = self.Z
         d = Z[0,0] * Z[1,1] - Z[0,1] * Z[1,0]
-        return npy.array([[-Z[0,0] / Z[1,0], -d / Z[1,0]],
+        return npy.array([[Z[0,0] / Z[1,0], d / Z[1,0]],
                           [1.0 / Z[1,0], Z[1,1] / Z[1,0]]])
     
     @property
     def Y(self):
         """Return Z-parameter matrix"""
-        return npy.invert(self.Z)
+        return npy.linalg.inv(self.Z)
 
     @property
     def S(self, z0 = 50.0):
@@ -176,19 +176,20 @@ class NPortZ(NPort):
 
     @property
     def CY(self):
-        Y = mat(self.Y)
+        Y = npy.mat(self.Y)
         return Y * self.CZ * Y.H
 
     @property
-    def CS(self):
+    def CS(self, z0 = 50.):
+        S = npy.mat(self.S)
         E = npy.mat(npy.eye(self.n, self.n))
-        return (E - S) * npy.mat(self.CZ) * (E - S).H / 4
+        T = (E - S) / (2 * npy.sqrt(z0))
+        return  T * npy.mat(self.CZ) * T.H
 
     @property
     def CA(self):
-        T = npy.matrix([[1, -self.A[0,0]], [0, -self.A[1,0]]])
-
-        return T * npy.mat(self.CZ) * T.H
+       T = npy.matrix([[1, -self.A[0,0]], [0, -self.A[1,0]]])
+       return T * npy.mat(self.CZ) * T.H
 
 class NPortA(NPort):
     """Two-port class where the internal representation is the ABCD-parameters"""
@@ -196,16 +197,18 @@ class NPortA(NPort):
     def __init__(self, A, CA = None):
         if isinstance(A, NPort):
             self.A = A.A
+            self.CA = A.CA
         else:
             self.A = A
+
+            if CA == None:
+                self.CA = npy.zeros(npy.shape(self.Y))
+            else:
+                self.CA = CA
 
         if npy.shape(self.A) != (2,2):
             raise ValueError('Can only create ABCD-two ports')
         
-        if CA == None:
-            self.CA = npy.zeros(npy.shape(self.Y))
-        else:
-            self.CA = CA
 
         self.n = 2
 
@@ -236,14 +239,15 @@ class NPortA(NPort):
                               [  4.20000000e-03,   1.59000000e+00]])
         >>> P = NPortA(abcd)
         >>> P.S
-        array([[ 0.1,  0.3,  0.5,  0.6]])
+        array([[ 0.1,  0.3],
+               [ 0.5,  0.6]])
 
         >>> 
         """
         a,b,c,d = self.A[0,0], self.A[0,1], self.A[1,0], self.A[1,1]
 
-        A = npy.array([[a + b / z0 - c * z0 - d, 2 * (a * d - b * c),
-                        2,                       -a+b/z0-c*z0+d]])
+        A = npy.array([[a + b / z0 - c * z0 - d, 2 * (a * d - b * c)],
+                       [2,                       -a+b/z0-c*z0+d]])
         return 1/(a + b / z0 + c * z0 + d) * A
 
     @property
@@ -261,9 +265,8 @@ class NPortA(NPort):
         return npy.array(T * npy.mat(self.CA) * T.H)
 
     @property
-    def CS(self):
-        E = npy.mat(npy.eye(self.n, self.n))
-        return npy.array((E - S) * npy.mat(self.CZ) * (E - S).H / 4)
+    def CS(self, z0=50.):
+        return NPortY(self).CS
 
     def __str__(self):
         return self.__class__.__name__ + '(' + repr(self.A) + ')'
@@ -277,13 +280,14 @@ class NPortS(NPort):
         
         if isinstance(S, NPort):
             self.S = S.S
+            self.CS = S.CS
         else:
             self.S = S
         
-        if CS == None:
-            self.CS = npy.zeros(npy.shape(self.S))
-        else:
-            self.CS = CS
+            if CS == None:
+                self.CS = npy.zeros(npy.shape(self.S))
+            else:
+                self.CS = CS
 
         self.n = npy.size(self.S,0)
 
@@ -305,7 +309,7 @@ class NPortS(NPort):
         c = 1 / z0 * ((1 - s[0,0]) * (1 - s[1,1]) - s[0,1]*s[1,0]) / (2 * s[1,0])
         d = ((1 - s[0,0]) * (1 + s[1,1]) + s[0,1]*s[1,0]) / (2 * s[1,0])
         
-        return npy.array([[a,b],[c,d]], dtype=object)
+        return npy.array([[a,b],[c,d]], object)
 
     @property
     def Z(self):
@@ -314,7 +318,7 @@ class NPortS(NPort):
         E = npy.mat(npy.eye(self.n, self.n))
         Zref = self.z0 * E
         Gref = 1 / npy.sqrt(npy.real(self.z0)) * E
-        return npy.array(Gref**-1 * (E - S)**-1 * (S + E) * Zref * Gref)
+        return npy.array(Gref.I * (E - S).I * (S + E) * Zref * Gref)
 
     @property
     def Y(self):
@@ -327,235 +331,28 @@ class NPortS(NPort):
 
     @property
     def CY(self):
-        Y = self.Y
+        Y = npy.mat(self.Y)
+        y0 = 1. / self.z0
         E = npy.mat(npy.eye(self.n, self.n))
-        T = E + Y
+        T = (y0 * E + Y) / npy.sqrt(y0)
         return T * npy.mat(self.CS) * T.H
 
     @property
     def CZ(self):
         Z = self.Z
         E = npy.mat(npy.eye(self.n, self.n))
-        T = E + Z
+        T = (self.z0 * E + Z) / npy.sqrt(self.z0)
         return T * npy.mat(self.CS) * T.H
 
     @property
     def CA(self):
-        T = npy.matrix([[1, -self.A[0,0]], [0, -self.A[1,0]]])
+        return NPortZ(self).CA
+        z0 = self.z0
+        A = npy.mat(self.A)
+        T = npy.matrix([[npy.sqrt(z0), -(A[0,1]+A[0,0]*z0)/npy.sqrt(z0)],
+                        [-1/npy.sqrt(z0), -(A[1,1]+A[1,0]*z0)/npy.sqrt(z0)]])
+        return  T * npy.mat(self.CS) * T.H
 
-        return T * npy.mat(self.CZ) * T.H
-
-class TwoPortAnalysis(Analysis):
-    """Analysis to find the 2-ports parameters of a circuit
-
-    The transmission parameters are found as:
-
-    A = v(inp, inn)/v(outp, outn) | io = 0
-    B = v(inp, inn)/i(outp, outn) | vo = 0
-    C = i(inp, inn)/v(outp, outn) | io = 0
-    D = i(inp, inn)/i(outp, outn) | vo = 0
-
-    >>> c = SubCircuit()
-    >>> n1 = c.add_node('net1')
-    >>> n2 = c.add_node('net2')
-    >>> c['R1'] = R(n1, n2, r=9e3)
-    >>> c['R2'] = R(n2, gnd, r=1e3)
-    >>> res = TwoPortAnalysis(c, n1, gnd, n2, gnd).solve(freqs = npy.array([0]))
-    >>> res['mu'].y[0]
-    (0.1+0j)
-    >>> res['gamma'].y[0]
-    (0.000111111111111+0j)
-    >>> res['zeta'].y[0]
-    (1000+0j)
-    >>> res['beta'].y[0]
-    (1+0j)
-    
-    """
-    
-    ACAnalysis = AC
-    NoiseAnalysis = Noise
-    
-    def __init__(self, circuit, inp, inn, outp, outn, noise = False, 
-                 noise_outquantity = 'v', method = 'sparam'):
-        self.c = circuit
-
-        self.ports = (inp, inn), (outp, outn)
-
-        self.noise = noise
-        self.noise_outquantity = noise_outquantity
-
-        self.method = 'sparam'
-        
-    def solve(self, freqs, complexfreq = False, refnode = gnd):
-        result = InternalResultDict()
-
-        if self.method == 'sparam':
-            result['twoport'] = self.solve_s(freqs, complexfreq=complexfreq)
-            abcd = result['twoport'].A
-        else:
-            abcd = self.solve_abcd(freqs, refnode=refnode, complexfreq=complexfreq)
-            result['twoport'] = NPortA(abcd)
-
-        result['mu'] = 1 / abcd[0,0]
-        result['gamma'] = 1 / abcd[0,1]
-        result['zeta'] = 1 / abcd[1,0]
-        result['beta'] = 1 / abcd[1,1]
-
-        if self.noise:
-            (inp, inn), (outp, outn) = self.ports
-            
-            circuit_vs = copy(self.c)
-            circuit_vs['VS_TwoPort'] = VS(inp, inn, vac = 1)
-            
-            circuit_cs = copy(self.c)
-            circuit_cs['IS_TwoPort'] = IS(inp, inn, iac = 1)
-            
-            if self.noise_outquantity == 'i':
-                for src in circuit_vs, circuit_cs:
-                    src['VL'] = VS(outp, outn, vac = 0)
-
-            if self.noise_outquantity == 'v':
-                res_v = self.NoiseAnalysis(circuit_vs, 
-                                           inputsrc=circuit_vs['VS_TwoPort'],
-                                           outputnodes=(outp, outn)
-                                           ).solve(freqs, complexfreq=complexfreq)
-
-                res_i = self.NoiseAnalysis(circuit_cs, 
-                                           inputsrc=circuit_cs['IS_TwoPort'],
-                                           outputnodes=(outp, outn)
-                                           ).solve(freqs, complexfreq=complexfreq)
-            else:
-                res_v = self.NoiseAnalysis(circuit_vs, 
-                                           inputsrc=circuit_vs['VS_TwoPort'],
-                                           outputsrc=circuit_vs['VL']
-                                           ).solve(freqs, complexfreq=complexfreq)
-
-                res_i = self.NoiseAnalysis(circuit_cs, 
-                                           inputsrc=circuit_cs['IS_TwoPort'],
-                                           outputsrc=circuit_cs['VL']
-                                           ).solve(freqs, complexfreq=complexfreq)
-
-            result['Svn'] = res_v['Svninp']
-            result['Sin'] = res_i['Sininp']
-            
-        self.result = result
-
-        return result
-
-    def solve_s(self, freqs, complexfreq = False):
-        """Calculate scattering (s) parameters of circuit
-
-        >>> c = SubCircuit()
-        >>> n1 = c.add_node('net1')
-        >>> n2 = c.add_node('net2')
-        >>> c['R1'] = R(n1, n2, r=9e3)
-        >>> c['R2'] = R(n2, gnd, r=1e3)
-
-
-        >>> an = TwoPortAnalysis(c, n1, gnd, n2, gnd)
-        >>> twoport = an.solve_s(freqs = 0)
-        >>> mu = 1/twoport.A[0,0]
-        >>> print mu
-        (0.1+0j)
-
-        """
-
-        ## The s-parameters of an n-port are defined as
-        ## B = S * A
-        ## where A and B are column-vectors of size n of 
-        ## the ingoing and outgoing waves respectively
-        ## S is an NxN matrix containing the s-parameters
-        ## The elements of the A and B vectors are defined as:
-        ## 
-        ## a_n = (v_n + Z0 * i_n) / 2 * 1 / sqrt(|Re Z0|)
-        ## b_n = (v_n - Z0 * i_n) / 2 * 1 / sqrt(|Re Z0|)
-        ## 
-        ## where v is the port-voltage and i the current flowing 
-        ## into the device, Z0 is an arbitrary chosen impedance
-        ##
-        ## The straight-forward method to calculate the S matrix
-        ## that is used here is to run n AC-analyses and in each
-        ## analysis connect a voltage source of voltage 2V
-        ## in series with a resistor with resistance R0 ohm
-        ## (The chosen Z0 value is real). The other ports
-        ## are terminated with resistance R0.
-        ## The s-parameters S_k_n can now be calculated as:
-        ## S_k_n = b_k / a_n
-        ## where
-        ## a_n = ((2-R0*i_n) + R0 * i+n) / 2 / sqrt(R0) = 1 / sqrt(R0)
-        ## b_k = (v_k + v_k) / 2 / sqrt(R0) = v_k / sqrt(R0) | k != n
-        ## b_n = ((2-R0*i_n) - R0*i_n) / 2 / sqrt(R0) = {i_n = (v_n - 2)/R0} = (2-2*R0*(v_n-2)/R0)/2/sqrt(R0) =
-        ##     = (1 - v_n - 2) / sqrt(R0) = (v_n - 1) / sqrt(R0)
-        ## => S_k_n = b_k / a_n = v_k | k != n
-        ## S_n_n = b_n / a_n = v_n - 1
-        ##
-        ## 
-        # Reference impedance
-        import sympy
-        r0 = 1
-
-        N = len(self.ports)
-
-        portnumbers = range(N)
-
-        S = npy.zeros((N,N), dtype=object)
-        
-        for n, sourceport in enumerate(self.ports):
-            circuit = copy(self.c)
-            
-            vs_plus = circuit.add_node('vs_plus')
-            
-            ## Power source at port n
-            circuit['_vs'] = VS(vs_plus, sourceport[1], vac = 2)
-            circuit['_rs'] = R(vs_plus, sourceport[0], r = r0)
-            
-            ## Terminate other ports
-            for k, port in enumerate(self.ports):
-                if port != sourceport:
-                    circuit['_rl%d'%k] = R(port[0], port[1], r = r0)
-            
-            ## Run AC-analysis
-            res = self.ACAnalysis(circuit).solve(freqs, refnode=sourceport[1],
-                                                 complexfreq = complexfreq)
-
-            ## Obtain s-parameters
-            for k, port in enumerate(self.ports):
-                if k == n:
-                    S[k,n] = res.v(port[0], port[1]) - 1
-                else:
-                    S[k,n] = res.v(port[0], port[1])
-
-        return NPortS(S, z0=r0)
-
-    def solve_abcd(self, freqs, refnode = gnd, complexfreq = False):
-        (inp, inn), (outp, outn) = self.ports
-                
-        ## Add voltage source at input port and create
-        ## copies with output open and shorted respectively
-        circuit_vs_open = copy(self.c)
-
-        circuit_vs_open['VS_TwoPort'] = VS(inp, inn, vac=1)
-
-        circuit_vs_shorted = copy(circuit_vs_open)
-
-        circuit_vs_shorted['VL_TwoPort'] = VS(outp, outn, vac=0)
-
-        ## Run AC-analysis on the two circuits
-        ac_open = self.ACAnalysis(circuit_vs_open)
-        ac_shorted = self.ACAnalysis(circuit_vs_shorted)
-
-        res_open = ac_open.solve(freqs, refnode = refnode, 
-                                 complexfreq=complexfreq)
-
-        res_shorted = ac_shorted.solve(freqs, refnode = refnode, 
-                                     complexfreq=complexfreq)
-        
-        A = res_open.v(inp, inn) / res_open.v(outp, outn)
-        B = res_shorted.v(inp, inn) / res_shorted.i('VL_TwoPort.plus')
-        C = res_open.i('VS_TwoPort.minus') / res_open.v(outp, outn)
-        D = res_shorted.i('VS_TwoPort.minus') / res_shorted.i('VL_TwoPort.plus')
-
-        return npy.array([[A,B],[C,D]], dtype=object)
 
 if __name__ == "__main__":
     import doctest
