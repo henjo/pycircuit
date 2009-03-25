f5be36eedb0fa32f10f58ac8f948a67927c7c554
Henrik Johansson <henjo2006@gmail.com>
2009-03-25 10:11:49 +0100

    Fixed doctests in waveform

--- pycircuit/utilities/rst.py
+++ pycircuit/utilities/rst.py
@@ -29,15 +29,17 @@ def table(rows, header=True, headerrows = 1, vdelim="  ", padding=1, justify='ri
     - justify - may be left,center,right
     """
 
-    s = '\n'
+    s = ''
     
     border="=" # character for drawing the border
-    justify = {'left':string.ljust,'center':string.center, 'right':string.rjust}[justify.lower()]
+    justify = {'left':string.ljust,'center':string.center, 
+               'right':string.rjust}[justify.lower()]
 
     # calculate column widhts (longest item in each col
     # plus "padding" nr of spaces on both sides)
     cols = zip(*rows)
-    colWidths = [max([len(unicode(item))+2*padding for item in col]) for col in cols]
+    colWidths = [max([len(unicode(item))+2*padding for item in col]) 
+                 for col in cols]
 
     # the horizontal border needed by rst
     borderline = vdelim.join([w*border for w in colWidths])
@@ -45,13 +47,16 @@ def table(rows, header=True, headerrows = 1, vdelim="  ", padding=1, justify='ri
     # outputs table in rst format
     s += borderline + '\n'
     for i, row in enumerate(rows):
-        if header:
+        if header and headerrows == i+1:
             justfunc = string.ljust
         else:
             justfunc = justify
-        s += vdelim.join([justfunc(unicode(item),width) for (item,width) in zip(row,colWidths)]).encode('utf-8') + '\n'
+
+        s += vdelim.join([justfunc(unicode(item),width) for (item,width) 
+                          in zip(row,colWidths)]).encode('utf-8') + '\n'
+
         if header and headerrows == i+1: s += borderline +'\n'
-    s += borderline + '\n'
+    s += borderline
     return s
 
 if __name__ == "__main__":
