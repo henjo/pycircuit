794bfeb06ce9431deff3bd7ddf04dde395cf61c1
Joacim Frisk <joacim@fundera.(none)>
2010-02-22 17:45:03 +0100

    Updated transient tests after transient change.

--- pycircuit/circuit/tests/test_transient.py
+++ pycircuit/circuit/tests/test_transient.py
@@ -4,12 +4,13 @@
 """Circuit element tests
 """
 
-from pycircuit.circuit.elements import VSin, IS, R, L, C, SubCircuit, gnd
+from pycircuit.circuit.elements import VSin, ISin, IS, R, L, C, SubCircuit, gnd
 from pycircuit.circuit.transient import Transient
 from pycircuit.circuit import circuit #new
 from math import floor
-from myCap import myC
+from pycircuit.circuit.myCap import myC
 import pylab
+import numpy as np
 from pycircuit.post import plotall
 
 def test_transient_RC():
@@ -21,13 +22,16 @@ def test_transient_RC():
 
     n1 = c.add_node('net1')
     n2 = c.add_node('net2')
-    c['Is'] = IS(gnd, n1, i=10)    
+    c['ISin'] = ISin(gnd, n1, ia=10, freq=100)    
     c['R1'] = R(n1, gnd, r=1)
     c['R2'] = R(n1, n2, r=1e3)
     c['R3'] = R(n2, gnd, r=100e3)
     c['C'] = C(n2, gnd, c=1e-5)
     tran = Transient(c)
     res = tran.solve(tend=10e-3,timestep=1e-4)
+    #plotall(res.v(n1),res.v(n2))
+    plotall(res.v(n2))
+    pylab.show()
     expected = 6.3
     assert  abs(res.v(n2,gnd)[-1] - expected) < 1e-2*expected,\
         'Does not match QUCS result.'
@@ -39,7 +43,7 @@ def test_transient_RLC():
     
     circuit.default_toolkit = circuit.numeric
     c = SubCircuit()
-
+    
     c['VSin'] = VSin(gnd, 1, va=10, freq=50e3)
     c['R1'] = R(1, 2, r=1e6)
     c['C'] = C(2, gnd, c=1e-12)
@@ -47,8 +51,8 @@ def test_transient_RLC():
     tran_imp = Transient(c)
     res_imp = tran_imp.solve(tend=40e-6,timestep=1e-6)
     expected = 2.58
-    #plotall(res_imp.v(1),res_imp.v(2))
-    #pylab.show()
+    plotall(res_imp.v(1),res_imp.v(2))
+    pylab.show()
     assert  abs(res_imp.v(2,gnd)[-1] - expected) < 1e-2*expected,\
         'Does not match QUCS result.'
 
@@ -58,7 +62,7 @@ def test_transient_nonlinear_C():
     """
     circuit.default_toolkit = circuit.numeric
     c = SubCircuit()
-
+    
     c['VSin'] = VSin(gnd, 1, va=10, freq=50e3)
     c['R1'] = R(1, 2, r=1e6)
     c['C'] = myC(2, gnd)
@@ -66,8 +70,8 @@ def test_transient_nonlinear_C():
     tran_imp = Transient(c)
     res_imp = tran_imp.solve(tend=40e-6,timestep=1e-6)
     expected = 3.4
-    #plotall(res_imp.v(1),res_imp.v(2))
-    #pylab.show()
+    plotall(res_imp.v(1),res_imp.v(2))
+    pylab.show()
     assert  abs(res_imp.v(2,gnd)[-1] - expected) < 1e-2*expected,\
         'Does not match QUCS result:'
 
@@ -76,15 +80,20 @@ def test_transient_get_diff():
     """
     circuit.default_toolkit = circuit.numeric
     c = SubCircuit()
+    c['VSin'] = VSin(gnd, 1, va=10, freq=50e3)
+    c['R1'] = R(1, 2, r=1e6)
+    c['C'] = C(2, gnd, c=1e-12)
     tran = Transient(c)
-    q=1
-    qlast=0.9
-    dt=1
-    result = tran.get_diff(q,qlast,dt,iqlast=0.1,method='trapezoidal')
-    assert abs(result-0.1) < 1e-6*result
+    tran._dt=1e-6
+    x0=np.ones(c.n)
+    q=c.q(x0)
+    Cmatrix=c.C(x0)
+    iq,geq = tran.get_diff(q,Cmatrix)
+    print iq,geq
+
 
 if __name__ == '__main__':
-    test_transient_RC()
+    #test_transient_RC()
     test_transient_RLC()
-    test_transient_nonlinear_C()
-    
+    #test_transient_nonlinear_C()
+    #test_transient_get_diff()
