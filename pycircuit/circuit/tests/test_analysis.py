34f13870cec78c2a9b423b3f670186897dbb071f
Henrik Johansson <henrik@johome.net>
2010-08-03 20:40:23 +0200

    Fixed bug in symbolic toolkit that make noise calculations use numeric temperature instead of 'T' symbol

--- pycircuit/circuit/tests/test_analysis.py
+++ pycircuit/circuit/tests/test_analysis.py
@@ -29,7 +29,7 @@ def test_symbolic_noise_vin_vout():
     pycircuit.circuit.circuit.default_toolkit = symbolic
     c = SubCircuit()
 
-    var('R1 R2 kT V', real=True, positive=True)
+    var('R1 R2 k T V', real=True, positive=True)
 
     c['vs'] = VS(1, gnd, vac=V)
     c['R1'] = R(1, 2, r=R1)
@@ -39,15 +39,15 @@ def test_symbolic_noise_vin_vout():
                   toolkit=symbolic)
     res = noise.solve(s, complexfreq=True)
 
-    assert_equal(simplify(res['Svnout']), simplify(4*R1*R2*kT/(R1 + R2)))
-    assert_equal(simplify(res['Svninp']), (4*R1*R2*kT + 4*kT*R1**2)/R2)
+    assert_equal(simplify(res['Svnout']), simplify(4*R1*R2*k*T/(R1 + R2)))
+    assert_equal(simplify(res['Svninp']), (4*R1*R2*k*T + 4*k*T*R1**2)/R2)
     assert_equal(simplify(res['gain'] - R2 / (R1 + R2)), 0)
 
 def test_symbolic_noise_vin_iout():
     pycircuit.circuit.circuit.default_toolkit = symbolic
     c = SubCircuit()
     
-    var('R1 R2 R3 kT V', real=True, positive=True)
+    var('R1 R2 R3 k T V', real=True, positive=True)
 
     c['vs'] = VS(1, gnd, vac=V)
     c['R1'] = R(1, 2, r=R1)
@@ -58,8 +58,8 @@ def test_symbolic_noise_vin_iout():
                   toolkit=symbolic)
     res = noise.solve(s, complexfreq=True)
     
-    assert_equal(simplify(res['Sinout']), simplify(4*kT*(R1+R2)/(R1*R2)))
-    assert_equal(simplify(res['Svninp']), (4*R1*R2*kT + 4*kT*R1**2)/R2)
+    assert_equal(simplify(res['Sinout']), simplify(4*k*T*(R1+R2)/(R1*R2)))
+    assert_equal(simplify(res['Svninp']), (4*R1*R2*k*T + 4*k*T*R1**2)/R2)
     assert_equal(simplify(res['gain']), 1/R1)
 
 def test_symbolic_noise_iin_vout():
@@ -67,7 +67,7 @@ def test_symbolic_noise_iin_vout():
     c = SubCircuit()
     
     var('R1 R2', real=True)
-    var('kT I')
+    var('k T I')
 
     c['is'] = IS(1, gnd, iac=I)
     c['R1'] = R(1, 2, r=R1)
@@ -77,8 +77,8 @@ def test_symbolic_noise_iin_vout():
                   toolkit=symbolic)
     res = noise.solve(s, complexfreq=True)
 
-    assert_equal(simplify(res['Svnout']), 4*R2*kT)
-    assert_equal(simplify(res['Sininp']), 4*kT/R2)
+    assert_equal(simplify(res['Svnout']), 4*R2*k*T)
+    assert_equal(simplify(res['Sininp']), 4*k*T/R2)
     assert_equal(simplify(res['gain']), R2)
 
 
@@ -87,7 +87,7 @@ def test_symbolic_noise_iin_iout():
     c = SubCircuit()
     
     var('R1 R2 R3', real=True)
-    var('kT I')
+    var('k T I s')
 
     c['is'] = IS(1, gnd, iac=I)
     c['R1'] = R(1, 2, r=R1)
@@ -98,15 +98,15 @@ def test_symbolic_noise_iin_iout():
                   toolkit=symbolic)
     res = noise.solve(s, complexfreq=True)
 
-    assert_equal(simplify(res['Sinout']), 4*kT/R2)
-    assert_equal(simplify(res['Sininp']), 4*kT/R2)
+    assert_equal(simplify(res['Sinout']), 4*k*T/R2)
+    assert_equal(simplify(res['Sininp']), 4*k*T/R2)
     assert_equal(simplify(res['gain']), 1)
 
 def test_symbolic_noise_kt_over_C():
     pycircuit.circuit.circuit.default_toolkit = symbolic
     cir = SubCircuit()
 
-    var('r c w w1 kT V', real=True, positive=True)
+    var('r c w w1 k T V', real=True, positive=True)
 
     s = 1j * w
 
@@ -119,9 +119,9 @@ def test_symbolic_noise_kt_over_C():
     res = noise.solve(s, complexfreq=True)
 
 
-    noise_voltage_power = gruntz(integrate(simplify(res['Svnout']), 
-                                           (w, 0, w1)), w1, oo)
+    noise_voltage_power = limit(integrate(simplify(res['Svnout']), 
+                                          (w, 0, w1)), w1, oo)
 
-    assert_equal(noise_voltage_power, 2*pi*kT/c)
+    assert_equal(noise_voltage_power, 2*pi*k*T/c)
 
     assert_equal(simplify(res['gain'] - 1/(1 + s*r*c)), 0)
