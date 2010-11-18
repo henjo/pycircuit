bd82a9a977dd4af8f129577e4f44525902977f66
andreas <anddr@anddr-ThinkPad-T61.(none)>
2010-11-18 10:52:29 +0100

    test_analysis.py: split into test_analysis_numeric.py and test_analysis_symbolic.py

---
+++ pycircuit/circuit/tests/test_analysis_numeric.py
@@ -0,0 +1,32 @@
+# -*- coding: latin-1 -*-
+# Copyright (c) 2008 Pycircuit Development Team
+# See LICENSE for details.
+
+from nose.tools import *
+import pycircuit.circuit.circuit 
+from pycircuit.circuit import *
+from pycircuit.circuit import symbolic
+import numpy as np
+from numpy.testing import assert_array_almost_equal, assert_array_equal
+from copy import copy
+from test_circuit import create_current_divider
+
+def test_integer_component_values():
+    """Test dc analysis with integer component values
+    
+       As python per default uses integer arithmetics integer
+       component values can lead to problems. (Python < 3.0)
+    """
+    pycircuit.circuit.circuit.default_toolkit = numeric
+    c = SubCircuit(toolkit=numeric)
+
+    n1,n2 = c.add_nodes('net1', 'net2')
+
+    c['vs'] = VS(n1, gnd, v = 9)
+    c['R1'] = R( n1,  n2, r = 50)
+    c['R2'] = R( n2, gnd, r = 50)
+
+    dc = DC(c)
+    res = dc.solve()
+    
+    assert_equal(res.v('net1'), 9.0)
