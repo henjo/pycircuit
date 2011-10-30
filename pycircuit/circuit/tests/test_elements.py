3e9d99ea487b2add391b8300e564b839011f6808
Joacim Olsson <joaco@sirius.(none)>
2011-10-30 10:48:14 +0100

    Added an integrator element idt()

--- pycircuit/circuit/tests/test_elements.py
+++ pycircuit/circuit/tests/test_elements.py
@@ -15,6 +15,9 @@ from numpy.testing.decorators import slow
 from sympy import var, Symbol, simplify, symbols
 import sympy
 
+from pylab import plot, show
+from pycircuit.circuit.transient import Transient
+
 def test_vsin():
     var('vo va freq td theta phase t')
     vsin = VSin(toolkit = symbolic,
@@ -242,6 +245,66 @@ def test_SVCVS_laplace_d3_n1():
 
     assert_equal(sympy.simplify(res.v(n2,gnd)),
                  sympy.simplify((b0*s*s)/(a0*s*s*s+a1*s*s+a2*s+a3)))
+
+def test_Idt_sym():
+    """Test integrator element symbolically"""
+    pycircuit.circuit.circuit.default_toolkit = symbolic
+
+    c = SubCircuit()
+
+    Vin = Symbol('Vin')
+    R1 = Symbol('R1')
+    
+    nin = c.add_node('in')
+    nout = c.add_node('out')
+     
+    c['vin'] = VS(nin, gnd, vac=Vin)
+    c['R1'] = R(nout, gnd, r=R1)
+    c['Idt'] = Idt(nin, gnd, nout, gnd)
+    
+    result = AC(c, toolkit=symbolic).solve(Symbol('s'),complexfreq=True)
+    
+    vtr = simplify(result.v(nout)/result.v(nin))
+    assert_equal(vtr, 1/Symbol('s'))
+
+def test_Idt_tran():
+    """Test integrator element in transient"""
+    pycircuit.circuit.circuit.default_toolkit = numeric
+
+    c = SubCircuit()
+    nin = c.add_node('in')
+    nout = c.add_node('out')
+     
+    c['vin'] = VSin(nin, gnd, va=1., freq=1e3)
+    c['R1'] = R(nout, gnd, r=1e3)
+    c['Idt'] = Idt(nin, gnd, nout, gnd)
+    
+    tran = Transient(c)
+    result = tran.solve(tend=10e-4,timestep=1e-5)
+    plot(result.v(nin),result.v(nout))
+    #plotall(result.v(nin),result.v(nout))
+    #plot(result.v(nout))
+    show()
+
+def test_Idtmod_tran_():
+    """Test modulo integrator element in transient"""
+    pycircuit.circuit.circuit.default_toolkit = numeric
+
+    c = SubCircuit()
+    nin = c.add_node('in')
+    nout = c.add_node('out')
+     
+    c['vin'] = VS(nin, gnd, v=100.)
+    c['R1'] = R(nout, gnd, r=1e3)
+    c['Idtmod'] = Idt(nin, gnd, nout, gnd, modulus = 1., offset = -0.5)
+    
+    tran = Transient(c)
+    result = tran.solve(tend=10e-3,timestep=1e-5)
+    plot(result.v(nin),result.v(nout))
+    #plotall(result.v(nin),result.v(nout))
+    #plot(result.v(nout))
+    show()
+
 
 if __name__ == '__main__':
     test_nullor_vva()
